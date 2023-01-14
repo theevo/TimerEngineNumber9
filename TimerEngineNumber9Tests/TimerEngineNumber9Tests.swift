53532a25b44093156a25d15aea8841f936da2fd0
Theo Vora <theo.vora@gmail.com>
2023-01-14 18:53:06 +0700

    refactor: remove unneeded booleans. to inquire the timer's state, simply get state

--- TimerEngineNumber9Tests/TimerEngineNumber9Tests.swift
+++ TimerEngineNumber9Tests/TimerEngineNumber9Tests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 final class TimerEngineNumber9Tests: XCTestCase {
 
-    func test_Timer_canReceive3SecondDuration() throws {
+    func test_Timer_canSet3SecondDuration() throws {
         let timer = TENTimer(3)
         
         let duration = timer.duration
@@ -18,13 +18,13 @@ final class TimerEngineNumber9Tests: XCTestCase {
         XCTAssertEqual(duration, 3)
     }
     
-    func test_Timer_canStart() async {
+    func test_Timer_entersStartedStateWhenTimerStarted() {
         let timer = TENTimer(1)
         timer.start()
-        XCTAssertEqual(timer.didStart, true)
+        XCTAssertEqual(timer.state, .started)
     }
     
-    func test_Timer_canCountdownFrom1Second() async {
+    func test_Timer_completesCountdownFrom1Second() {
         let timer = TENTimer(1)
 
         timer.start()
@@ -32,7 +32,7 @@ final class TimerEngineNumber9Tests: XCTestCase {
         let exp = expectation(description: "Test after 1 second")
         let result = XCTWaiter.wait(for: [exp], timeout: 1.05)
         if result == XCTWaiter.Result.timedOut {
-            XCTAssertEqual(timer.didFinish, true)
+            XCTAssertEqual(timer.state, .finished)
         } else {
             XCTFail("Delay interrupted")
         }
@@ -44,13 +44,6 @@ class TENTimer {
     /// duration in seconds
     public var duration: UInt
     public var state: State = .notStarted
-    
-    public var didStart: Bool {
-        state == .started
-    }
-    public var didFinish: Bool {
-        state == .finished
-    }
     
     var doubleDuration: Double {
         Double(duration)
