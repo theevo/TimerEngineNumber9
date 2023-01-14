545dacaf003d508784f8f0a99354fbba6c9a47a3
Theo Vora <theo.vora@gmail.com>
2023-01-14 15:38:16 +0700

    initialize TENTimer with duration

--- TimerEngineNumber9Tests/TimerEngineNumber9Tests.swift
+++ TimerEngineNumber9Tests/TimerEngineNumber9Tests.swift
@@ -10,27 +10,20 @@ import XCTest
 
 final class TimerEngineNumber9Tests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_Timer_canReceive3SecondDuration() throws {
+        let timer = TENTimer(3)
+        
+        let duration = timer.duration
+        
+        XCTAssertEqual(duration, 3)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+struct TENTimer {
+    public var duration: UInt
+    
+    public init(_ duration: UInt) {
+        self.duration = duration
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
