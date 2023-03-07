9f001aed892bb3f1625ad28f0a6050f7cc5d22d3
Theo Vora <theo.vora@gmail.com>
2023-03-07 15:33:39 +0700

    Create framework for TENTimer

---
+++ TENTimer/TENTimer/TENTimer.h
@@ -0,0 +1,18 @@
+//
+//  TENTimer.h
+//  TENTimer
+//
+//  Created by Theo Vora on 3/7/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TENTimer.
+FOUNDATION_EXPORT double TENTimerVersionNumber;
+
+//! Project version string for TENTimer.
+FOUNDATION_EXPORT const unsigned char TENTimerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TENTimer/PublicHeader.h>
+
+
