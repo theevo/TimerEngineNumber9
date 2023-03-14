f47764e68dd894ca495e13e88ec1e759051564fb
Theo Vora <theo.vora@gmail.com>
2023-03-14 15:08:55 +0700

    Move `PlayPauseButton` to its own file

--- UIKit-Timer/UIKit-Timer/TimerViewController.swift
+++ UIKit-Timer/UIKit-Timer/TimerViewController.swift
@@ -8,41 +8,6 @@
 import UIKit
 import TENTimer
 
-class PlayPauseButton: UIButton {
-    enum Icon: String {
-        case Play = "play.circle.fill"
-        case Pause = "pause.circle.fill"
-    }
-    
-    var icon: Icon = .Play
-    
-    init() {
-        super.init(frame: .zero)
-        translatesAutoresizingMaskIntoConstraints = false
-        updateView()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func toggle() {
-        icon = icon == .Play ? .Pause : .Play
-        updateView()
-    }
-    
-    func updateView() {
-        configuration = setConfiguration()
-    }
-    
-    private func setConfiguration() -> UIButton.Configuration {
-        var config = UIButton.Configuration.plain()
-        config.image = UIImage(systemName: icon.rawValue)
-        config.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(pointSize: 80)
-        return config
-    }
-}
-
 class TimerViewController: UIViewController {
     
     var timer = TENTimer(minutes: 25)
